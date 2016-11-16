66f1409a54b57b455f00c7eae78c4c68049db609
edifierd <fedetubaro@hotmail.com>
2016-11-16 01:35:03 -0300

    Se agrego buscador de gauchada por localidad y destacados

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   def index
-  	@favor = Favor.all.where("estado='t'").order('id DESC')
-  	#
+  	if params[:ubicacion]
+  		@ubicacion = params[:ubicacion]
+  		@favor = Favor.all.where("estado='t' and ubicacion='"+@ubicacion+"'").order('id DESC')
+  		@ubicacion = " en "+params[:ubicacion]
+  		@destacados = Favor.all.where("estado='t'").order('id DESC').limit(3)
+  	else
+  		@favor = Favor.all.where("estado='t'").order('id DESC')
+  	end
   end
+
 end
