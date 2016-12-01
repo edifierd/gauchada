18c2f5b0f8f851f96e3d604b2f141236d0ebdd9c
edifierd <fedetubaro@hotmail.com>
2016-12-01 17:03:44 -0300

    Correccion de seguridad en el controlador del administrador

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -11,34 +11,44 @@ class AdminPanelController < ApplicationController
   def recaudacion
   	#dato = params["pay"]["inicio(1i)"].to_s
   	#flash[:danger] = "Inicio ."+inicio.to_s+". y la de fin: "+fin.to_s
-    if current_user.has_role? :admin
-  	if (params["send"] == 'true')  
-  		@inicio = Date.new(  params["pay"]["inicio(1i)"].to_i,
-                         	params["pay"]["inicio(2i)"].to_i,
-                          	params["pay"]["inicio(3i)"].to_i)
+    if current_user
+      if current_user.has_role? :admin
+  	     if (params["send"] == 'true')  
+  		      @inicio = Date.new(  params["pay"]["inicio(1i)"].to_i,
+                               	 params["pay"]["inicio(2i)"].to_i,
+                                 params["pay"]["inicio(3i)"].to_i)
 
-  		@fin = Date.new(     params["pay"]["fin(1i)"].to_i,
-                         	params["pay"]["fin(2i)"].to_i,
-                          	params["pay"]["fin(3i)"].to_i)
-  		if @fin > @inicio
-        	@pays = Pay.where("created_at >= '"+@inicio.to_s+"' and created_at <= '"+@fin.to_s+"'")
-        else 
-        	flash[:danger] = "Las fechas seleccionadas son incorrectas."
-        	redirect_to (:back)
-        end
-    end
+  		      @fin = Date.new(      params["pay"]["fin(1i)"].to_i,
+                                	params["pay"]["fin(2i)"].to_i,
+                                	params["pay"]["fin(3i)"].to_i)
+  		      if @fin > @inicio
+        	    @pays = Pay.where("created_at >= '"+@inicio.to_s+"' and created_at <= '"+@fin.to_s+"'")
+            else 
+        	    flash[:danger] = "Las fechas seleccionadas son incorrectas."
+        	    redirect_to (:back)
+            end
+          end
+      else
+    	  flash[:danger] = "Usted no es administrador"
+    	  redirect_to ("/")
+      end
     else
-    	flash[:danger] = "Usted no es administrador"
-    	redirect_to ("/")
+      flash[:danger] = "Debe acceder al sistema para ver esta informacion."
+      redirect_to ("/")
     end
   end
 
   def rankingEslabones
-  	if current_user.has_role? :admin
-  		@users = User.order('eslabon DESC')
-  	else 
-    	flash[:danger] = "Usted no es administrador"
-    	redirect_to ("/")
+    if current_user
+  	  if current_user.has_role? :admin
+  		  @users = User.order('eslabon DESC')
+  	  else 
+    	 flash[:danger] = "Usted no es administrador"
+    	 redirect_to ("/")
+      end
+    else
+      flash[:danger] = "Debe acceder al sistema para ver esta informacion."
+      redirect_to ("/")
     end
   end
 
